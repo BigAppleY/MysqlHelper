5dbc7c8da70f3eef263941e360c918a9c2a0f8e4
AppY <568748457@qq.com>
2021-04-14 14:36:05 +0800

    update

---
+++ DataHelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace dsbsstj.Helper
+{
+    public abstract class DataHelper
+    {
+    
+        private static readonly string connectionString = "data source=localhost;database=wedge_lottery_mg_monit;user id=root;password=frontfree;pooling=true;charset=utf8;";
+        public static string GetConfig()
+        {
+           
+                return connectionString;
+         
+        }
+
+        /// <summary>
+        /// Dataset转换为实体集合
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="ds"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public static List<T> DataSetAToEntitiyList<T>(DataSet ds, int index = 0)
+        {
+            List<T> list = new List<T>();
+            if (ds == null || ds.Tables.Count <= 0)
+            {
+                return list;
+            }
+            else if (index > ds.Tables.Count - 1)
+            {
+                return list;
+            }
+            else if (index < 0)
+            {
+                index = 0;
+            }
+            else if (ds.Tables[index].Rows.Count <= 0)
+            {
+                return list;
+            }
+
+            DataTable dt = ds.Tables[index];
+            for (int i = 0, len = dt.Rows.Count; i < len; i++)
+            {
+                T t = (T)Activator.CreateInstance(typeof(T));
+                PropertyInfo[] propertys = t.GetType().GetProperties();
+                foreach (PropertyInfo p in propertys)
+                {
+                    if (dt.Columns.IndexOf(p.Name.ToUpper()) != -1 && dt.Rows[i][p.Name.ToUpper()] != DBNull.Value)
+                    {
+                        p.SetValue(t, dt.Rows[i][p.Name.ToUpper()], null);
+                    }
+                    else
+                    {
+                        p.SetValue(t, null, null);
+                    }
+
+                }
+                list.Add(t);
+            }
+            return list;
+
+        }
+    }
+}
---
+++ MySqlHelper.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace dsbsstj.Helper
+{
+    public abstract class MySqlHelper
+    {
+        #region Property Field
+        /// <summary>
+        /// 批量操作每批次记录数
+        /// </summary>
+        public static int BatchSize = 2000;
+
+        /// <summary>
+        /// 超时时间
+        /// </summary>
+        public static int CommandTimeOut = 600;
+
+
+        #endregion
+
+
+        #region 静态方法
+
+        #region ExecuteDataSet
+
+        /// <summary>
+        /// 执行SQL语句,返回结果集
+        /// </summary>
+        /// <param name="connectionString">数据库连接字符串</param>
+        /// <param name="commandText">SQL语句</param>
+        /// <param name="parms">查询参数</param>
+        /// <returns>返回结果集</returns>
+        public static DataSet ExecuteDataSet(string connectionString, string commandText, params MySqlParameter[] parms)
+        {
+            return ExecuteDataSet(connectionString, CommandType.Text, commandText, parms);
+        }
+
+        /// <summary>
+        /// 执行SQL语句,返回结果集
+        /// </summary>
+        /// <param name="connectionString">数据库连接字符串</param>
+        /// <param name="commandType">命令类型(存储过程,命令文本, 其它.)</param>
+        /// <param name="commandText">SQL语句或存储过程名称</param>
+        /// <param name="parms">查询参数</param>
+        /// <returns>返回结果集</returns>
+        public static DataSet ExecuteDataSet(string connectionString, CommandType commandType, string commandText, params MySqlParameter[] parms)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                return ExecuteDataSet(connection, commandType, commandText, parms);
+            }
+        }
+
+        /// <summary>
+        /// 执行SQL语句,返回结果集
+        /// </summary>
+        /// <param name="connection">数据库连接</param>
+        /// <param name="commandType">命令类型(存储过程,命令文本, 其它.)</param>
+        /// <param name="commandText">SQL语句或存储过程名称</param>
+        /// <param name="parms">查询参数</param>
+        /// <returns>返回结果集</returns>
+        public static DataSet ExecuteDataSet(MySqlConnection connection, CommandType commandType, string commandText, params MySqlParameter[] parms)
+        {
+            return ExecuteDataSet(connection, null, commandType, commandText, parms);
+        }
+
+        /// <summary>
+        /// 执行SQL语句,返回结果集
+        /// </summary>
+        /// <param name="transaction">事务</param>
+        /// <param name="commandType">命令类型(存储过程,命令文本, 其它.)</param>
+        /// <param name="commandText">SQL语句或存储过程名称</param>
+        /// <param name="parms">查询参数</param>
+        /// <returns>返回结果集</returns>
+        public static DataSet ExecuteDataSet(MySqlTransaction transaction, CommandType commandType, string commandText, params MySqlParameter[] parms)
+        {
+            return ExecuteDataSet(transaction.Connection, transaction, commandType, commandText, parms);
+        }
+
+        /// <summary>
+        /// 执行SQL语句,返回结果集
+        /// </summary>
+        /// <param name="connection">数据库连接</param>
+        /// <param name="transaction">事务</param>
+        /// <param name="commandType">命令类型(存储过程,命令文本, 其它.)</param>
+        /// <param name="commandText">SQL语句或存储过程名称</param>
+        /// <param name="parms">查询参数</param>
+        /// <returns>返回结果集</returns>
+        private static DataSet ExecuteDataSet(MySqlConnection connection, MySqlTransaction transaction, CommandType commandType, string commandText, params MySqlParameter[] parms)
+        {
+            MySqlCommand command = new MySqlCommand();
+
+            PrepareCommand(command, connection, transaction, commandType, commandText, parms);
+            MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            if (commandText.IndexOf("@") > 0)
+            {
+                commandText = commandText.ToLower();
+                int index = commandText.IndexOf("where ");
+                if (index < 0)
+                {
+                    index = commandText.IndexOf("\nwhere");
+                }
+                if (index > 0)
+                {
+                    ds.ExtendedProperties.Add("SQL", commandText.Substring(0, index - 1));  //将获取的语句保存在表的一个附属数组里，方便更新时生成CommandBuilder
+                }
+                else
+                {
+                    ds.ExtendedProperties.Add("SQL", commandText);  //将获取的语句保存在表的一个附属数组里，方便更新时生成CommandBuilder
+                }
+            }
+            else
+            {
+                ds.ExtendedProperties.Add("SQL", commandText);  //将获取的语句保存在表的一个附属数组里，方便更新时生成CommandBuilder
+            }
+
+            foreach (DataTable dt in ds.Tables)
+            {
+                dt.ExtendedProperties.Add("SQL", ds.ExtendedProperties["SQL"]);
+            }
+
+            command.Parameters.Clear();
+            return ds;
+        }
+
+        #endregion ExecuteDataSet
+
+
+
+        #endregion
+
+        #region PrepareCommand
+
+        /// <summary>
+        /// PrepareCommand
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="connection"></param>
+        /// <param name="transaction"></param>
+        /// <param name="commandType"></param>
+        /// <param name="commandText"></param>
+        /// <param name="parms"></param>
+        private static void PrepareCommand(MySqlCommand command, MySqlConnection connection,
+            MySqlTransaction transaction, CommandType commandType, string commandText, MySqlParameter[] parms)
+        {
+            if (connection.State != ConnectionState.Open) connection.Open();
+
+            command.Connection = connection;
+            command.CommandTimeout = CommandTimeOut;
+            // 设置命令文本(存储过程名或SQL语句)
+            command.CommandText = commandText;
+            // 分配事务
+            if (transaction != null)
+            {
+                command.Transaction = transaction;
+            }
+            // 设置命令类型.
+            command.CommandType = commandType;
+            if (parms != null && parms.Length > 0)
+            {
+                //预处理MySqlParameter参数数组，将为NULL的参数赋值为DBNull.Value;
+                foreach (MySqlParameter parameter in parms)
+                {
+                    if ((parameter.Direction == ParameterDirection.InputOutput ||
+                         parameter.Direction == ParameterDirection.Input) && (parameter.Value == null))
+                    {
+                        parameter.Value = DBNull.Value;
+                    }
+                }
+                command.Parameters.AddRange(parms);
+            }
+        }
+
+        #endregion
+
+
+    }
+}
